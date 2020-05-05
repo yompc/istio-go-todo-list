2fbb057121eb34cc5a7903ba48e95fcce3aec305
liufengjia <yom535@outlook.com>
2020-05-05 14:52:47 +0800

    update

--- web/todo-list/src/utils/api.js
+++ web/todo-list/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import router from '@/router';
 
 axios.defaults.timeout = 5000;
-axios.defaults.baseURL ='http://184.172.234.86:30080/api/v1'; //填写域名
+axios.defaults.baseURL ='https://todo-list-api-tau.now.sh/api/v1'; //填写域名
 
 //http request 拦截器
 axios.interceptors.request.use(
